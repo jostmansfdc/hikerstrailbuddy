ea8f8f9a0090d1087a40a5367bbee0963ba7fa6a
Felipe Ryan <feliperyan@gmail.com>
2018-05-14 14:16:38 +0200

    try again

--- standup_schema.js
+++ standup_schema.js
@@ -7,6 +7,6 @@ const client = new Client({
 client.connect()
 
 client.query('CREATE TABLE \"session\" (\"sid" varchar NOT NULL COLLATE \"default\", \"sess\" json NOT NULL, \"expire\" timestamp(6) NOT NULL) WITH (OIDS=FALSE); ALTER TABLE \"session\" ADD CONSTRAINT \"session_pkey\" PRIMARY KEY (\"sid\") NOT DEFERRABLE INITIALLY IMMEDIATE;', (err, res) => {
-    console.log(err ? err.stack : res.rows[0].message) // Hello World!
+    console.log('DONE');
     client.end()
 })
